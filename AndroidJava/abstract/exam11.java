a9f797e45a133bb94bf369b930642dce71208064
msseock <msseock@gmail.com>
2021-05-14 22:01:02 +0900

    add polymorphism example

---
+++ AndroidJava/abstract/exam11.java
@@ -0,0 +1,12 @@
+// 다형성(polymorphism) 예
+public class exam11 {
+	public static void main(String[] args) {
+		Animal animal;
+		
+		animal = new Tiger();
+		animal.move();
+		
+		animal = new Eagle();
+		animal.move();
+	}
+}
